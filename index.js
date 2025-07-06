6038965e71d7c90be7aa271d19f83d6755ad17e7
Abdul Samad <137094595+AbdulSamad001122@users.noreply.github.com>
2025-07-06 16:44:19 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const urlRoute = require("./routes/url");
+const { connectToMongoDB } = require("./connect");
+const URL = require("./models/url");
+const path = require("path");
+
+const app = express();
+const port = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/url-shortner").then(
+  console.log("MongoDB Connected")
+);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+
+  if (!entry) {
+    return res.status(404).json({ error: "URL not found" });
+  }
+
+  res.redirect(entry.redirectUrl);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
